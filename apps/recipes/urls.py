6c6db67615e44994cd6496e99fcd23c472653b67
Bryce Boley <bryceboley1@gmail.com>
2015-02-18 22:30:21 -0700

    got photos directed to correct folder and all working right

--- apps/recipes/urls.py
+++ apps/recipes/urls.py
@@ -9,8 +9,7 @@ urlpatterns = patterns(
     url(r'^recipes/(?P<pk>[0-9]+)$', RecipeDetail.as_view(), name='recipe-list'),
     url(r'^add-recipe$', AddRecipe.as_view(), name='add-recipe'),
 
-	url(r'^ingredients/$', IngredientsList.as_view(), name='ingredients-list'),
-
-# Handling media files
-	url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    url(r'^ingredients/$', IngredientsList.as_view(), name='ingredients-list'),
+    # Handling media files
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
--- recipe_organizer/settings.py
+++ recipe_organizer/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'recipe_organizer.urls'
 
 WSGI_APPLICATION = 'recipe_organizer.wsgi.application'
 
-MEDIA_ROOT = '/Users/simplymac/PycharmProjects/recipe_organizer/backend/apps/recipes/media'
+MEDIA_ROOT = '/Users/simplymac/recipe_project/recipe_organizer/backend/apps/recipes/media'
 
 MEDIA_URL = '/media/'
 # Database
