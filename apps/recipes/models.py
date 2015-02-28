1fc7cf58243faba6951826c699e8d082c85dc1b0
Bryce Boley <bryceboley1@gmail.com>
2015-02-28 15:24:56 -0700

    added static_root

--- apps/recipes/models.py
+++ apps/recipes/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.name
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -18,9 +18,9 @@ class Tag(models.Model):
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True, help_text="This is a quick description of your recipe")
-    directions = models.TextField(help_text="How to make the recipe")
-    ingredients = models.ManyToManyField(Ingredient)
-    tags = models.ManyToManyField(Tag)
+    directions = models.TextField(blank=True, null=True, help_text="How to make the recipe")
+    ingredients = models.ManyToManyField(Ingredient, blank=True, null=True)
+    tags = models.ManyToManyField(Tag, blank=True, null=True)
     photo = models.ImageField(upload_to='photos', blank=True, null=True)
 
     def __str__(self):
