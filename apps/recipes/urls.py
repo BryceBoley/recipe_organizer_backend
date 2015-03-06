75b959c3f9f7e1dd5da982d47ae30a1ce59f784c
Bryce Boley <bryceboley1@gmail.com>
2015-03-05 22:22:07 -0700

    changed serializers, views, and urls for photo add and edit functionality

--- apps/recipes/urls.py
+++ apps/recipes/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns(
     '',
 
     url(r'^recipes/$', RecipeList.as_view(), name='recipe-list'),
-    url(r'^recipes/(?P<pk>[0-9]+)$', RecipeDetail.as_view(), name='recipe-list'),
+    url(r'^recipes/(?P<pk>[0-9]+)$', RecipeDetail.as_view(), name='recipe-detail'),
     url(r'^add-recipe$', AddRecipe.as_view(), name='add-recipe'),
 
     url(r'^ingredients/$', IngredientsList.as_view(), name='ingredients-list'),
