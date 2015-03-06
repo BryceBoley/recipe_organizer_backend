80d5ab516987494b8fa290821e5026aff7a2469e
Bryce Boley <bryceboley1@gmail.com>
2015-03-06 03:55:45 -0700

    changed url add edit recipe, might not use it though

--- apps/recipes/urls.py
+++ apps/recipes/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns(
     url(r'^recipes/$', RecipeList.as_view(), name='recipe-list'),
     url(r'^recipes/(?P<pk>[0-9]+)$', RecipeDetail.as_view(), name='recipe-detail'),
     url(r'^add-recipe$', AddRecipe.as_view(), name='add-recipe'),
+    url(r'^edit-recipe$', RecipeDetail.as_view(), name='edit-recipe'),
 
     url(r'^ingredients/$', IngredientsList.as_view(), name='ingredients-list'),
     # Handling media files
