b465e4f56b9fbfc1f266157bbaa795cd14835aa2
Bryce Boley <bryceboley1@gmail.com>
2015-03-10 11:50:53 -0600

    update MEDIA_ROOT in url

--- recipe_organizer/settings.py
+++ recipe_organizer/settings.py
@@ -85,7 +85,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-STATIC_ROOT = '/Users/simplymac/recipe_project/recipe_organizer/backend/staticfiles'
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 CORS_ORIGIN_WHITELIST = (
