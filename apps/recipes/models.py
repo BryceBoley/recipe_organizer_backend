c5bb38a8b28f81407478a4f9aee1252be4205d2a
Bryce Boley <bryceboley1@gmail.com>
2015-04-07 15:39:04 -0600

    changed models photo required false

--- apps/recipes/models.py
+++ apps/recipes/models.py
@@ -2,26 +2,26 @@ from django.db import models
 
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=100, blank=True, null=True)
+    name = models.CharField(max_length=25, blank=True)
 
     def __str__(self):
         return self.name
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=50, blank=True, null=True)
+    name = models.CharField(max_length=25, blank=True)
 
     def __str__(self):
         return self.name
 
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True, help_text="This is a quick description of your recipe")
-    directions = models.TextField(blank=True, null=True, help_text="How to make the recipe")
+    name = models.CharField(max_length=40)
+    description = models.TextField(blank=True, help_text="This is a quick description of your recipe")
+    directions = models.TextField(blank=True, help_text="How to make the recipe")
     ingredients = models.ManyToManyField(Ingredient, blank=True, null=True)
     tags = models.ManyToManyField(Tag, blank=True, null=True)
-    photo = models.ImageField(upload_to='photos', blank=True, null=True)
+    photo = models.ImageField(upload_to='photos', required=False, blank=True, null=True)
 
     def __str__(self):
         return self.name
